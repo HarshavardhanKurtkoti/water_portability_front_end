eb5bab649cdd84b0eeab6985e971d4542827509a
Harshavardhan <kurtkoti.harsha@gmail.com>
2025-04-22 23:57:20 +0530

    modified:   src/Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -17,8 +17,8 @@ const Contact = () => {
     setAlert({ show: false, text: '', type: '' });
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: 'Harshavardhan Kurtkoti',
@@ -26,7 +26,7 @@ const Contact = () => {
           to_email: 'kurtkoti.harsha@gmail.com',
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
       )
       .then(
         () => {
